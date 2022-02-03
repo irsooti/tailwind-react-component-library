9d01660a0080a0210e63a2fe0ab8869698ab5136
Daniele <irsooti@gmail.com>
2022-02-03 13:02:01 +0100

    scaffold

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import classes from './Button.module.css';
+
+export interface ButtonProps {
+  label: string;
+}
+
+export function Button(props: ButtonProps) {
+  useEffect(() => {
+    console.log(props);
+  }, [props]);
+
+  return <button className={classes.root}>{props.label}</button>;
+}
